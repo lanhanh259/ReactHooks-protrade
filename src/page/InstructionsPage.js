5ca1ac5e7c19d26f9d6f219e10e56d75a41e7ac4
IPA\hanh.nguyenlan <IPA\hanh.nguyenlan>
2022-03-24 14:42:37 +0700

    lib :styled-component

--- src/page/InstructionsPage.js
+++ src/page/InstructionsPage.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import './asset.css'
+// import './asset.css'
+import styled from 'styled-components'
 
 const InstructionsPage = () => {
 	return (
-		<div id="guide">
-			<div style={{ padding: '60px' }}>
-				<h1 className="txt-orange fw-bold text-uppercase">
-					HƯỚNG DẪN SỬ DỤNG DTRADEPRO
-				</h1>
-				<p className="text-uppercase fw-bold">
-					Tốc độ vượt trội - Chớp ngay cơ hội
-				</p>
-				<div className="bag-white"></div>
+		<Container>
+			<div id="guide">
+				<div style={{ padding: '60px' }}>
+					<h1 className="txt-orange fw-bold text-uppercase">
+						HƯỚNG DẪN SỬ DỤNG DTRADEPRO
+					</h1>
+					<p className="text-uppercase fw-bold">
+						Tốc độ vượt trội - Chớp ngay cơ hội
+					</p>
+					<div className="bag-white"></div>
+				</div>
 			</div>
-		</div>
+		</Container>
 	)
 }
-
+const Container = styled.div`
+	#guide {
+		padding-top: 60px;
+		height: 100vh;
+		background-image: url(https://static-01.vndirect.com.vn/images/protrade/guide/background_guide.jpg);
+		background-size: cover;
+		background-position: 50%;
+	}
+`
 export default InstructionsPage
